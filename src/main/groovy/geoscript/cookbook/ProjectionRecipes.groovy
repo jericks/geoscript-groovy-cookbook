5b068bfe844ae95bc568a39be68c8c4e2711ebe2
jericks <jared.erickson@gmail.com>
2017-01-01 13:58:46 -0800

    Add recipes for geodetic and decimal degreesa

--- src/main/groovy/geoscript/cookbook/ProjectionRecipes.groovy
+++ src/main/groovy/geoscript/cookbook/ProjectionRecipes.groovy
@@ -1,9 +1,12 @@
 package geoscript.cookbook
 
 import geoscript.geom.Bounds
+import geoscript.proj.DecimalDegrees
+import geoscript.proj.Geodetic
 import geoscript.proj.Projection
 import geoscript.geom.Point
 import geoscript.geom.Geometry
+import org.geotools.referencing.CRS
 
 class ProjectionRecipes extends Recipes {
 
@@ -39,6 +42,18 @@ class ProjectionRecipes extends Recipes {
         // end::createFromName[]
         writeFile("projection_createprojectionfromname", proj.wkt)
         proj
+    }
+
+    List<Projection> getAllProjections() {
+        CRS.metaClass.static.getSupportedCodes = { String type ->
+            [4326,4269,26918,2263,2927]
+        }
+        // tag::getAllProjections[]
+        List<Projection> projections = Projection.projections()
+        // end::getAllProjections[]
+        CRS.metaClass = null
+        writeFile("projection_projections", projections.join(NEW_LINE) + NEW_LINE + "...")
+        projections
     }
 
     Map<String, Object> getProperties() {
@@ -120,5 +135,223 @@ class ProjectionRecipes extends Recipes {
         // end::transformStaticProj[]
         writeFile("projection_transformstaticproj", epsg2927Geom.wkt)
         epsg2927Geom
+    }
+
+    Geodetic createGeodetic() {
+        // tag::createGeodetic[]
+        Geodetic geodetic = new Geodetic("wgs84")
+        println geodetic
+        // end::createGeodetic[]
+        writeFile("projection_geodetic", "${geodetic}")
+        geodetic
+    }
+
+    Map inverseGeodetic() {
+        // tag::inverseGeodetic1[]
+        Geodetic geodetic = new Geodetic("clrk66")
+        Point bostonPoint = new Point(-71.117, 42.25)
+        Point portlandPoint = new Point(-123.683, 45.52)
+        Map results = geodetic.inverse(bostonPoint, portlandPoint)
+        double forwardAzimuth =  results.forwardAzimuth
+        println forwardAzimuth
+        // end::inverseGeodetic1[]
+        writeFile("projection_geodetic_inverse_1", "${forwardAzimuth}")
+        // tag::inverseGeodetic2[]
+        double backAzimuth = results.backAzimuth
+        println backAzimuth
+        // end::inverseGeodetic2[]
+        writeFile("projection_geodetic_inverse_2", "${backAzimuth}")
+        // tag::inverseGeodetic3[]
+        double distance = results.distance
+        println distance
+        // end::inverseGeodetic3[]
+        writeFile("projection_geodetic_inverse_3", "${distance}")
+        results
+    }
+
+    Map forwardGeodetic() {
+        // tag::forwardGeodetic1[]
+        Geodetic geodetic = new Geodetic("clrk66")
+        Point bostonPoint = new Point(-71.117, 42.25)
+        Map results = geodetic.forward(bostonPoint, -66.531, 4164192.708)
+        Point point = results.point
+        println point
+        // end::forwardGeodetic1[]
+        writeFile("projection_geodetic_forward_1", results.point.wkt)
+        // tag::forwardGeodetic2[]
+        double azimuth = results.backAzimuth
+        println azimuth
+        // end::forwardGeodetic2[]
+        writeFile("projection_geodetic_forward_2", "${azimuth}")
+        results
+    }
+
+    List<Point> placePointsGeodetic() {
+        // tag::placePointsGeodetic[]
+        Geodetic geodetic = new Geodetic("clrk66")
+        Point bostonPoint = new Point(-71.117, 42.25)
+        Point portlandPoint = new Point(-123.683, 45.52)
+        List<Point> points = geodetic.placePoints(bostonPoint, portlandPoint, 10)
+        points.each { Point point ->
+            println point.wkt
+        }
+        // end::placePointsGeodetic[]
+        writeFile("projection_geodetic_points", points.join(NEW_LINE))
+        points
+    }
+
+    DecimalDegrees createDecimalDegreesFromLongitudeAndLatitude() {
+        // tag::createDecimalDegreesFromLongitudeAndLatitude[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees(-122.525619, 47.212023)
+        println decimalDegrees
+        // end::createDecimalDegreesFromLongitudeAndLatitude[]
+        writeFile("projection_decimaldegrees_longitude_latitude", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromPoint() {
+        // tag::createDecimalDegreesFromPoint[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees(new Point(-122.525619,47.212023))
+        println decimalDegrees
+        // end::createDecimalDegreesFromPoint[]
+        writeFile("projection_decimaldegrees_point", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromLongitudeLatitudeString() {
+        // tag::createDecimalDegreesFromLongitudeLatitudeString[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("-122.525619, 47.212023")
+        println decimalDegrees
+        // end::createDecimalDegreesFromLongitudeLatitudeString[]
+        writeFile("projection_decimaldegrees_longitudelatitudestring", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromTwoStringsWithGlyphs() {
+        // tag::createDecimalDegreesFromTwoStringsWithGlyphs[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122\u00B0 31' 32.23\" W", "47\u00B0 12' 43.28\" N")
+        println decimalDegrees
+        // end::createDecimalDegreesFromTwoStringsWithGlyphs[]
+        writeFile("projection_decimaldegrees_twostrings_glyphs", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromTwoStringsWithCharacters() {
+        // tag::createDecimalDegreesFromTwoStringsWithCharacters[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W", "47d 12m 43.28s N")
+        println decimalDegrees
+        // end::createDecimalDegreesFromTwoStringsWithCharacters[]
+        writeFile("projection_decimaldegrees_twostrings_characters", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromOneDMSString() {
+        // tag::createDecimalDegreesFromOneDMSString[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W, 47d 12m 43.28s N")
+        println decimalDegrees
+        // end::createDecimalDegreesFromOneDMSString[]
+        writeFile("projection_decimaldegrees_dmsstring", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromOneDDMGlyphsString() {
+        // tag::createDecimalDegreesFromOneDDMGlyphsString[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122\u00B0 31.5372' W, 47\u00B0 12.7213' N")
+        println decimalDegrees
+        // end::createDecimalDegreesFromOneDDMGlyphsString[]
+        writeFile("projection_decimaldegrees_ddmglyphstring", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    DecimalDegrees createDecimalDegreesFromOneDDMString() {
+        // tag::createDecimalDegreesFromOneDDMString[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31.5372m W, 47d 12.7213m N")
+        println decimalDegrees
+        // end::createDecimalDegreesFromOneDDMString[]
+        writeFile("projection_decimaldegrees_ddmstring", "${decimalDegrees}")
+        decimalDegrees
+    }
+
+    Map decimalDegreesGetDms() {
+        // tag::decimalDegreesGetDms1[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W", "47d 12m 43.28s N")
+        Map dms = decimalDegrees.dms
+        println "Degrees: ${dms.longitude.degrees}"
+        println "Minutes: ${dms.longitude.minutes}"
+        println "Seconds: ${dms.longitude.seconds}"
+        // end::decimalDegreesGetDms1[]
+        writeFile("projection_decimaldegrees_dms_1", "Degrees: ${dms.longitude.degrees}" + NEW_LINE +
+                "Minutes: ${dms.longitude.minutes}" + NEW_LINE +
+                "Seconds: ${dms.longitude.seconds}"
+        )
+        // tag::decimalDegreesGetDms2[]
+        println "Degrees: ${dms.latitude.degrees}"
+        println "Minutes: ${dms.latitude.minutes}"
+        println "Seconds: ${dms.latitude.seconds}"
+        // end::decimalDegreesGetDms2[]
+        writeFile("projection_decimaldegrees_dms_2", """Degrees: ${dms.latitude.degrees}
+Minutes: ${dms.latitude.minutes}
+Seconds: ${dms.latitude.seconds}""")
+        dms
+    }
+
+    Map decimalDegreesToDms() {
+        // tag::decimalDegreesToDms1[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W", "47d 12m 43.28s N")
+        println decimalDegrees.toDms(true)
+        // end::decimalDegreesToDms1[]
+        writeFile("projection_decimaldegrees_todms1", decimalDegrees.toDms(true))
+        // tag::decimalDegreesToDms2[]
+        println decimalDegrees.toDms(false)
+        // end::decimalDegreesToDms2[]
+        writeFile("projection_decimaldegrees_todms2", decimalDegrees.toDms(false))
+        return [
+                true:  decimalDegrees.toDms(true),
+                false: decimalDegrees.toDms(false)
+        ]
+    }
+
+    Map decimalDegreesGetDdm() {
+        // tag::decimalDegreesGetDdm1[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W", "47d 12m 43.28s N")
+        Map dms = decimalDegrees.ddm
+        println "Degrees: ${dms.longitude.degrees}"
+        println "Minutes: ${dms.longitude.minutes}"
+        // end::decimalDegreesGetDdm1[]
+        writeFile("projection_decimaldegrees_ddm_1", "Degrees: ${dms.longitude.degrees}" + NEW_LINE +
+                "Minutes: ${dms.longitude.minutes}"
+        )
+        // tag::decimalDegreesGetDdm2[]
+        println "Degrees: ${dms.latitude.degrees}"
+        println "Minutes: ${dms.latitude.minutes}"
+        // end::decimalDegreesGetDdm2[]
+        writeFile("projection_decimaldegrees_ddm_2", """Degrees: ${dms.latitude.degrees}
+Minutes: ${dms.latitude.minutes}""")
+        dms
+    }
+
+    Map decimalDegreesToDdm() {
+        // tag::decimalDegreesToDdm1[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W", "47d 12m 43.28s N")
+        println decimalDegrees.toDdm(true)
+        // end::decimalDegreesToDdm1[]
+        writeFile("projection_decimaldegrees_toddm1", decimalDegrees.toDdm(true))
+        // tag::decimalDegreesToDdm2[]
+        println decimalDegrees.toDdm(false)
+        // end::decimalDegreesToDdm2[]
+        writeFile("projection_decimaldegrees_toddm2", decimalDegrees.toDdm(false))
+        return [
+                true:  decimalDegrees.toDdm(true),
+                false: decimalDegrees.toDdm(false)
+        ]
+    }
+
+    Point decimalDegreesGetPoint() {
+        // tag::decimalDegreesGetPoint[]
+        DecimalDegrees decimalDegrees = new DecimalDegrees("122d 31m 32.23s W", "47d 12m 43.28s N")
+        Point point = decimalDegrees.point
+        // end::decimalDegreesGetPoint[]
+        writeFile("projection_decimaldegrees_point", "${point.wkt}")
+        point
     }
 }
--- src/test/groovy/geoscript/cookbook/ProjectionRecipesTest.groovy
+++ src/test/groovy/geoscript/cookbook/ProjectionRecipesTest.groovy
@@ -1,6 +1,9 @@
 package geoscript.cookbook
 
 import geoscript.geom.Geometry
+import geoscript.geom.Point
+import geoscript.proj.DecimalDegrees
+import geoscript.proj.Geodetic
 import geoscript.proj.Projection
 import org.junit.Test
 import static org.junit.Assert.*
@@ -23,6 +26,12 @@ class ProjectionRecipesTest {
         ProjectionRecipes recipes = new ProjectionRecipes()
         Projection proj = recipes.createFromName()
         assertTrue(proj.wkt.contains("Mollweide"))
+    }
+
+    @Test void getAllProjections() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        List<Projection> projections = recipes.getAllProjections()
+        assertFalse(projections.isEmpty())
     }
 
     @Test void getProperties() {
@@ -68,6 +77,129 @@ class ProjectionRecipesTest {
         ProjectionRecipes recipes = new ProjectionRecipes()
         Geometry geom = recipes.transformStaticStr()
         assertEquals("POINT (1158609.2040371667 703068.0661327887)", geom.wkt)
+    }
+
+    @Test void createGeodetic() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Geodetic geodetic = recipes.createGeodetic()
+        assertEquals('Geodetic [SPHEROID["WGS 84", 6378137.0, 298.257223563]]', geodetic.toString())
+    }
+
+    @Test void inverseGeodetic() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Map results = recipes.inverseGeodetic()
+        assertEquals(-66.53, results.forwardAzimuth, 0.1)
+        assertEquals(75.65, results.backAzimuth, 0.1)
+        assertEquals(4164050.459, results.distance, 0.1)
+    }
+
+    @Test void forwardGeodetic() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Map results = recipes.forwardGeodetic()
+        assertEquals("POINT (-123.6835797667373 45.516427795897236)", results.point.wkt)
+        assertEquals(75.65, results.backAzimuth, 0.1)
+    }
+
+    @Test void placePointsGeodetic() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        List<Point> points = recipes.placePointsGeodetic()
+        assertEquals(10, points.size())
+    }
+
+    @Test void createDecimalDegreesFromLongitudeAndLatitude() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromLongitudeAndLatitude()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromPoint() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromPoint()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromLongitudeLatitudeString() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromLongitudeLatitudeString()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromTwoStringsWithGlyphs() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromTwoStringsWithGlyphs()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromTwoStringsWithCharacters() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromTwoStringsWithCharacters()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromOneDMSString() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromOneDMSString()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromOneDDMGlyphsString() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromOneDDMGlyphsString()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void createDecimalDegreesFromOneDDMString() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        DecimalDegrees decimalDegrees = recipes.createDecimalDegreesFromOneDDMString()
+        assertEquals(-122.525619, decimalDegrees.longitude, 0.00001)
+        assertEquals(47.212023, decimalDegrees.latitude, 0.00001)
+    }
+
+    @Test void decimalDegreesGetDms() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Map dms = recipes.decimalDegreesGetDms()
+        assertEquals(-122, dms.longitude.degrees, 0.1)
+        assertEquals(31, dms.longitude.minutes, 0.1)
+        assertEquals(32.229, dms.longitude.seconds, 0.1)
+        assertEquals(47, dms.latitude.degrees, 0.1)
+        assertEquals(12, dms.latitude.minutes, 0.1)
+        assertEquals(43.280, dms.latitude.seconds, 0.1)
+    }
+
+    @Test void decimalDegreesToDms() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Map dms = recipes.decimalDegreesToDms()
+        assertEquals("-122\u00B0 31' 32.2300\" W, 47\u00B0 12' 43.2800\" N", dms.true)
+        assertEquals("-122d 31m 32.2300s W, 47d 12m 43.2800s N", dms.false)
+    }
+
+    @Test void decimalDegreesGetDdm() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Map ddm = recipes.decimalDegreesGetDdm()
+        assertEquals(-122, ddm.longitude.degrees, 0.1)
+        assertEquals(31.537, ddm.longitude.minutes, 0.1)
+        assertEquals(47, ddm.latitude.degrees, 0.1)
+        assertEquals(12.721, ddm.latitude.minutes, 0.1)
+    }
+
+    @Test void decimalDegreesToDdm() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Map ddm = recipes.decimalDegreesToDdm()
+        assertEquals("-122\u00B0 31.5372' W, 47\u00B0 12.7213' N", ddm.true)
+        assertEquals("-122d 31.5372m W, 47d 12.7213m N", ddm.false)
+    }
+
+    @Test void decimalDegreesGetPoint() {
+        ProjectionRecipes recipes = new ProjectionRecipes()
+        Point point = recipes.decimalDegreesGetPoint()
+        assertEquals("POINT (-122.52561944444444 47.212022222222224)", point.wkt)
     }
 
 }
