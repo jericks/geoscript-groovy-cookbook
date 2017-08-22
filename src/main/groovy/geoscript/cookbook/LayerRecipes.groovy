b519eb309f11f50fbf1584ab29748fc8c7b08d18
jericks <jared.erickson@gmail.com>
2017-08-21 19:45:44 -0700

    Start adding Layer recipes

--- src/main/groovy/geoscript/cookbook/LayerRecipes.groovy
+++ src/main/groovy/geoscript/cookbook/LayerRecipes.groovy
@@ -1,4 +1,31 @@
 package geoscript.cookbook
 
+import geoscript.layer.Layer
+import geoscript.workspace.GeoPackage
+import geoscript.workspace.Workspace
+
 class LayerRecipes extends Recipes {
+
+    Map<String, Object> getLayerProperties() {
+
+        Map<String, Object> values = [:]
+
+        // tag::getLayerProperties_name[]
+        Workspace workspace = new GeoPackage("src/main/resources/data.gpkg")
+        Layer layer = workspace.get("countries")
+        String name = layer.name
+        println "Name: ${name}"
+        // end::getLayerProperties_name[]
+        values.put("name", name)
+        writeFile("layer_properties_name", "Name: ${name}")
+
+        // tag::getLayerProperties_count[]
+        int count = layer.count
+        println "# of Features: ${count}"
+        // end::getLayerProperties_count[]
+        values.put("count", count)
+        writeFile("layer_properties_count", "# of Features: ${count}")
+
+        values
+    }
 }
--- src/test/groovy/geoscript/cookbook/LayerRecipesTest.groovy
+++ src/test/groovy/geoscript/cookbook/LayerRecipesTest.groovy
@@ -4,4 +4,12 @@ import org.junit.Test
 import static org.junit.Assert.*
 
 class LayerRecipesTest {
+
+    @Test void getLayerProperties() {
+        LayerRecipes recipes = new LayerRecipes()
+        Map<String, Object> values = recipes.getLayerProperties()
+        assertEquals("countries", values.name)
+        assertEquals(177, values.count)
+    }
+
 }
