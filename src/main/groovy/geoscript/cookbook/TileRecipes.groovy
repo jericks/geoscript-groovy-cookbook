6a5e7d6c82c9e72169dbbc7e03d6a614ee501a68
jericks <jared.erickson@gmail.com>
2018-02-12 19:03:04 -0800

    Add mbtiles recipe

--- src/main/groovy/geoscript/cookbook/TileRecipes.groovy
+++ src/main/groovy/geoscript/cookbook/TileRecipes.groovy
@@ -1,4 +1,37 @@
 package geoscript.cookbook
 
+import geoscript.geom.Bounds
+import geoscript.layer.MBTiles
+import geoscript.layer.TileLayer
+import geoscript.proj.Projection
+
 class TileRecipes extends Recipes {
+
+    MBTiles getMBTiles() {
+        // tag::getMBTiles[]
+        File file = new File("src/main/resources/tiles.mbtiles")
+        MBTiles mbtiles = new MBTiles(file)
+        // end::getMBTiles[]
+
+        // tag::getMBTiles_name[]
+        String name = mbtiles.name
+        println name
+        // end::getMBTiles_name[]
+        writeFile("tiles_get_mbtiles_name", "${name}")
+
+        // tag::getMBTiles_bounds[]
+        Bounds bounds = mbtiles.bounds
+        println bounds
+        // end::getMBTiles_bounds[]
+        writeFile("tiles_get_mbtiles_bounds", "${bounds}")
+
+        // tag::getMBTiles_proj[]
+        Projection proj = mbtiles.proj
+        println proj
+        // end::getMBTiles_proj[]
+        writeFile("tiles_get_mbtiles_proj", "${proj}")
+
+        mbtiles
+    }
+
 }
--- src/test/groovy/geoscript/cookbook/TileRecipesTest.groovy
+++ src/test/groovy/geoscript/cookbook/TileRecipesTest.groovy
@@ -1,7 +1,17 @@
 package geoscript.cookbook
 
+import geoscript.layer.MBTiles
 import org.junit.Test
 import static org.junit.Assert.*
 
 class TileRecipesTest {
+
+    @Test void getMBTiles() {
+        TileRecipes recipes = new TileRecipes()
+        MBTiles mbtiles = recipes.getMBTiles()
+        assertEquals("countries", mbtiles.name)
+        assertEquals("(-2.0036395147881314E7,-2.0037471205137067E7,2.0036395147881314E7,2.003747120513706E7,EPSG:3857)", mbtiles.bounds.toString())
+        assertEquals("EPSG:3857", mbtiles.proj.id)
+    }
+
 }
