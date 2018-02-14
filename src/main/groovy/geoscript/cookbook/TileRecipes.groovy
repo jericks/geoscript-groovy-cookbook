47a7e7643d7166ac19271db502af6d2a3c407a6b
jericks <jared.erickson@gmail.com>
2018-02-13 20:10:19 -0800

    Add pyramid and tile to mbtiles recipe

--- src/main/groovy/geoscript/cookbook/TileRecipes.groovy
+++ src/main/groovy/geoscript/cookbook/TileRecipes.groovy
@@ -1,7 +1,10 @@
 package geoscript.cookbook
 
 import geoscript.geom.Bounds
+import geoscript.layer.ImageTile
 import geoscript.layer.MBTiles
+import geoscript.layer.Pyramid
+import geoscript.layer.Tile
 import geoscript.layer.TileLayer
 import geoscript.proj.Projection
 
@@ -30,6 +33,19 @@ class TileRecipes extends Recipes {
         println proj
         // end::getMBTiles_proj[]
         writeFile("tiles_get_mbtiles_proj", "${proj}")
+
+        // tag::getMBTiles_pyramid[]
+        Pyramid pyramid = mbtiles.pyramid
+        println pyramid
+        // end::getMBTiles_pyramid[]
+        writeFile("tiles_get_mbtiles_pyramid", "${pyramid}")
+
+        // tag::getMBTiles_tile[]
+        Tile tile = mbtiles.get(0,0,0)
+        println tile
+        // end::getMBTiles_tile[]
+        writeFile("tiles_get_mbtiles_tile", "${tile}")
+        saveImage("tiles_get_mbtiles_tile", (tile as ImageTile).image)
 
         mbtiles
     }
