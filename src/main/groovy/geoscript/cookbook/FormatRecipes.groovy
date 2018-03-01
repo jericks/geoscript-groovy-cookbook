2224e8d3bab0fc5c4dcf649c980703a3713acef3
jericks <jared.erickson@gmail.com>
2018-02-28 18:38:31 -0800

    Add recipe for reading a raster from a format

--- src/main/groovy/geoscript/cookbook/FormatRecipes.groovy
+++ src/main/groovy/geoscript/cookbook/FormatRecipes.groovy
@@ -1,4 +1,18 @@
 package geoscript.cookbook
 
+import geoscript.layer.Format
+import geoscript.layer.Raster
+
 class FormatRecipes extends Recipes {
+
+    Raster read() {
+        // tag::read[]
+        File file = new File("src/main/resources/earth.tif")
+        Format format = Format.getFormat(file)
+        Raster raster = format.read("earth")
+        // end::read[]
+        draw("format_read", [raster])
+        raster
+    }
+
 }
--- src/test/groovy/geoscript/cookbook/FormatRecipesTest.groovy
+++ src/test/groovy/geoscript/cookbook/FormatRecipesTest.groovy
@@ -1,7 +1,15 @@
 package geoscript.cookbook
 
+import geoscript.layer.Raster
 import org.junit.Test
 import static org.junit.Assert.*
 
 class FormatRecipesTest {
+
+    @Test void read() {
+        FormatRecipes recipes = new FormatRecipes()
+        Raster raster = recipes.read()
+        assertNotNull(raster)
+    }
+
 }
