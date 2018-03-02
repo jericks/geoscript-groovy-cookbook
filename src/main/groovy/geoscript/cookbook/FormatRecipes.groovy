605b0d4047cd5671727bae9156546fada31be60c
jericks <jared.erickson@gmail.com>
2018-03-01 21:02:42 -0800

    Add a recipes for getting a format from a file and for checking if a format has a raster

--- src/main/groovy/geoscript/cookbook/FormatRecipes.groovy
+++ src/main/groovy/geoscript/cookbook/FormatRecipes.groovy
@@ -5,6 +5,31 @@ import geoscript.layer.Raster
 
 class FormatRecipes extends Recipes {
 
+    Format getFormat() {
+        // tag::getFormat[]
+        File file = new File("src/main/resources/earth.tif")
+        Format format = Format.getFormat(file)
+        println format.name
+        // end::getFormat[]
+        writeFile("format_get", "${format.name}")
+        format
+    }
+
+    Format hasRaster() {
+        // tag::hasRaster[]
+        File file = new File("src/main/resources/earth.tif")
+        Format format = Format.getFormat(file)
+
+        boolean hasEarth = format.has("earth")
+        println "Has raster named earth? ${hasEarth}"
+
+        boolean hasWorld = format.has("world")
+        println "Has raster named world? ${hasWorld}"
+        // end::hasRaster[]
+        writeFile("format_has", "Has raster named earth? ${hasEarth}${NEW_LINE}Has raster named world? ${hasWorld}")
+        format
+    }
+
     Raster read() {
         // tag::read[]
         File file = new File("src/main/resources/earth.tif")
--- src/test/groovy/geoscript/cookbook/FormatRecipesTest.groovy
+++ src/test/groovy/geoscript/cookbook/FormatRecipesTest.groovy
@@ -1,10 +1,24 @@
 package geoscript.cookbook
 
+import geoscript.layer.Format
 import geoscript.layer.Raster
 import org.junit.Test
 import static org.junit.Assert.*
 
 class FormatRecipesTest {
+
+    @Test void getFormat() {
+        FormatRecipes recipes = new FormatRecipes()
+        Format format = recipes.getFormat()
+        assertEquals("GeoTIFF", format.name)
+    }
+
+    @Test void hasRaster() {
+        FormatRecipes recipes = new FormatRecipes()
+        Format format = recipes.hasRaster()
+        assertTrue(format.has("earth"))
+        assertFalse(format.has("world"))
+    }
 
     @Test void read() {
         FormatRecipes recipes = new FormatRecipes()
